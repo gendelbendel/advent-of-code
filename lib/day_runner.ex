d7e87933d3169de65fcb77d24ee34388644756ec
David Ward <spy.sappin.your.sentry@gmail.com>
2021-12-16 18:10:12 +0100

    Remove logging statements and comments

--- lib/day_runner.ex
+++ lib/day_runner.ex
@@ -5,18 +5,8 @@ defmodule Mix.Tasks.DayRunner do
 
   @shortdoc "Run supplied day and part"
   def run(args) do
-    # input = AdventOfCode2021.Input.get!(1, 2021)
-
-    # input
-    # |> part1()
-    # |> IO.inspect(label: "Part 1 Results")
-
     parse_options(args)
-    # |> tap(&IO.inspect/1)
     |> run_with_options()
-
-    # get_all_day_modules()
-    # |> IO.inspect()
   end
 
   defp parse_options(args) do
@@ -33,8 +23,6 @@ defmodule Mix.Tasks.DayRunner do
   end
 
   defp validate_options(options) do
-    # IO.inspect(options)
-
     case options do
       {{:day, _day}, {:part, _part}} -> options
       {{:day, _day}} -> {:error, "Missing part. Supply with `-p X` or `--part X`"}
@@ -52,19 +40,11 @@ defmodule Mix.Tasks.DayRunner do
 
     day_modules
     |> Enum.reduce(%{day: day, part: part}, &find_matching_day/2)
-    # |> tap(&IO.inspect/1)
     |> validate_day_and_part()
     |> run_day_and_part()
   end
 
   defp find_matching_day(mod, acc) do
-    # IO.inspect(mod)
-
-    # IO.inspect(Map.get(acc, "day"), label: "Acc quoted day")
-    # IO.inspect(Map.get(acc, :day), label: "Acc atom day")
-    # IO.inspect(Map.get(mod, "day"), label: "mod quoted day")
-    # IO.inspect(Map.get(mod, :day), label: "mod atom day")
-
     cond do
       Map.get(acc, :day) == Map.get(mod, :day) ->
         Map.put(acc, :mod, Map.get(mod, :mod))
@@ -85,8 +65,6 @@ defmodule Mix.Tasks.DayRunner do
   end
 
   defp validate_day_and_part(%{day: _day, part: part, mod: _mod} = full) do
-    # IO.inspect(full)
-
     case part do
       1 ->
         Map.replace(full, :part, :part1)
@@ -106,35 +84,12 @@ defmodule Mix.Tasks.DayRunner do
   defp get_all_day_modules() do
     with {:ok, list} <- :application.get_key(:advent_of_code_2021, :modules) do
       list
-      # Enum.filter(&(&1 |> Module.split() |> Enum.take(1) == ~w|Day|))
       |> Enum.filter(&filter_day_modules_suffix/1)
-      # |> Enum.map(&get_day_from_module/1)
       |> Enum.map(&get_module_map_value/1)
     end
   end
 
-  defp get_variable_info(value) do
-    value
-    |> IEx.Info.info()
-    |> IO.inspect()
-  end
-
-  defp filter_day_modules(mod) do
-    # IO.inspect(IEx.Info.info(mod))
-    module_name =
-      mod
-      |> Atom.to_string()
-
-    String.contains?(module_name, ~w|Day|) && !String.contains?(module_name, ~w|Day|)
-
-    # case Atom.to_string(mod) do
-    #   ~w|Day| -> true
-    #   true -> false
-    # end
-  end
-
   defp filter_day_modules_suffix(mod) do
-    # IO.inspect(IEx.Info.info(mod))
     module_name =
       mod
       |> get_module_last_name()
@@ -142,11 +97,6 @@ defmodule Mix.Tasks.DayRunner do
     String.contains?(module_name, ~w|Day|) &&
       !String.contains?(module_name, ~w|Runner|) &&
       !String.contains?(module_name, ~w|Day00|)
-
-    # case Atom.to_string(mod) do
-    #   ~w|Day| -> true
-    #   true -> false
-    # end
   end
 
   defp get_module_map_value(mod) do
